3cdad0c96d3db3ff52401682425279f032447372
Ak all bots Control <akallbotscontrol@gmail.com>
2025-09-18 16:16:34 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -6,21 +6,25 @@ async def about_callback(client, query):
     await query.message.edit_text(
         text=(
             "╔════❰ 𝘼𝘽𝙊𝙐𝙏 𝙈𝙀 ❱════╗\n\n"
-            "○ **My Name :** LUCY BOT\n"
-            "○ **Creator :** AQUiB\n"
-            "○ **Library :** Pyrogram\n"
-            "○ **Language :** Python\n"
-            "○ **Database :** MongoDB\n"
-            "○ **Hosted On :** VPS\n"
-            "○ **Build Status :** v3 [Advance]\n\n"
+            "> **My Name :** LUCY BOT\n"
+            "> **Creator :** AQUiB\n"
+            "> **Library :** Pyrogram\n"
+            "> **Language :** Python\n"
+            "> **Database :** MongoDB\n"
+            "> **Hosted On :** VPS\n"
+            "> **Build Status :** v3 [Advance]\n\n\n"
+            "> Maintained by - [『 AK_BOTZ_UPDATE 』](https://t.me/AK_BOTZ_UPDATE)\n\n"
             "╚════════════════════╝"
         ),
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("‼️ DISCLAIMER ‼️", callback_data="disclaimer")],
-                [InlineKeyboardButton("• SUPPORT •", url="https://t.me/YourSupportGroup")],
-                [InlineKeyboardButton("• DEVELOPER •", url="https://t.me/YourUsername")],
-                [InlineKeyboardButton("• BACK •", callback_data="main")]
+                [
+                    InlineKeyboardButton("• SUPPORT •", url="https://t.me/YourSupportGroup"),
+                    InlineKeyboardButton("• DEVELOPER •", url="https://t.me/YourUsername")
+                ],
+                [InlineKeyboardButton("• BACK •", callback_data="home")]
             ]
-        )
+        ),
+        disable_web_page_preview=True  # hyperlink ka preview hide karega
     )
