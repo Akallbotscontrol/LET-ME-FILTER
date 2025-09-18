937cd7a59d3d474c4e62db483840b4e7d052b4eb
Ak all bots Control <akallbotscontrol@gmail.com>
2025-09-18 13:56:42 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,27 +1,26 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_callback_query()
+@Client.on_callback_query(filters.regex("^about$"))
 async def about_callback(client, query):
-    if query.data == "about":
-        await query.message.edit_text(
-            text=(
-                "╔════❰ 𝘼𝘽𝙊𝙐𝙏 𝙈𝙀 ❱════╗\n\n"
-                "○ **My Name :** LET_ME_FILTER\n"
-                "○ **Creator :** MR_ABHAY\n"
-                "○ **Library :** Pyrogram\n"
-                "○ **Language :** Python\n"
-                "○ **Database :** MongoDB\n"
-                "○ **Hosted On :** RENDER\n"
-                "○ **Build Status :** v3 [Advance]\n\n"
-                "╚════════════════════╝"
-            ),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("‼️ DISCLAIMER ‼️", callback_data="disclaimer")],
-                    [InlineKeyboardButton("• SUPPORT •", url="https://t.me/AK_BOTZ_SUPPORT")],
-                    [InlineKeyboardButton("• DEVELOPER •", url="https://t.me/MR_ABHAY_K")],
-                    [InlineKeyboardButton("• BACK •", callback_data="home")]
-                ]
-            )
+    await query.message.edit_text(
+        text=(
+            "╔════❰ 𝘼𝘽𝙊𝙐𝙏 𝙈𝙀 ❱════╗\n\n"
+            "○ **My Name :** LUCY BOT\n"
+            "○ **Creator :** AQUiB\n"
+            "○ **Library :** Pyrogram\n"
+            "○ **Language :** Python\n"
+            "○ **Database :** MongoDB\n"
+            "○ **Hosted On :** VPS\n"
+            "○ **Build Status :** v3 [Advance]\n\n"
+            "╚════════════════════╝"
+        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("‼️ DISCLAIMER ‼️", callback_data="disclaimer")],
+                [InlineKeyboardButton("• SUPPORT •", url="https://t.me/YourSupportGroup")],
+                [InlineKeyboardButton("• DEVELOPER •", url="https://t.me/YourUsername")],
+                [InlineKeyboardButton("• BACK •", callback_data="main")]
+            ]
         )
+    )
