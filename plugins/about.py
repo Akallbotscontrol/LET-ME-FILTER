aaedc47c1858952f5e063e56ff1f5650f14347db
Ak all bots Control <akallbotscontrol@gmail.com>
2025-09-19 13:05:08 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-from script import ABOUT_TXT   # text script.py se
+from Script import ABOUT_TXT   # text script.py se
 from command import ABOUT_BUTTONS   # buttons command.py se
 
 @Client.on_callback_query(filters.regex("about"))
